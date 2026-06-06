fa2c8adc8f7ad039149cdfdac94d6d2e670f02d9
Cris <cristiansabogals142@gmail.com>
2026-06-05 21:52:21 -0500

    primer proyecto

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <main style={{padding:"40px"}}>
+      <h1>Mi Estudio IA</h1>
+
+      <button>
+        Subir PDF
+      </button>
+
+      <br /><br />
+
+      <input
+        placeholder="Haz una pregunta"
+      />
+    </main>
+  );
+}
